7e983c78700e949e0600ec32fb605d97482ad63c
Antoni Matheus <125527018+antonimatheus@users.noreply.github.com>
2024-11-30 18:28:03 -0400

    added the function nav

--- script.js
+++ script.js
@@ -23,9 +23,13 @@ const navigation = document.querySelector('nav');
 window.addEventListener('scroll', () => {
     const intensity = Math.max(0, 1 - window.scrollY / (window.innerHeight * 0.4));
 
-    // Ajuste dinâmico da cor de fundo
-    navigation.style.backgroundColor = intensity < 0.5 ? 'white': '#F75A56';
-
-    // Ajuste dinâmico da cor do texto
+    // Ajuste dinâmico de cor para fundo e texto
+    navigation.style.backgroundColor = intensity < 0.5 ? 'white' : '#F75A56';
     navigation.style.color = intensity < 0.5 ? '#F75A56' : 'white';
+
+    // Ajuste dinâmico da cor dos links e ícones dentro do nav
+    const items = navigation.querySelectorAll('a, i');
+    items.forEach(item => {
+        item.style.color = intensity < 0.5 ? '#F75A56' : 'white';
+    });
 });
